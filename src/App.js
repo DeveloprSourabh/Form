8882867cb91b46fce70d3c5e8f41e55e030b2ac7
Sourabh <developr.sourabh@gmail.com>
2022-08-22 18:23:16 +0530

    add js code for getting value of input

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [fullName, setFullName] = useState({
+    fname: "",
+    lname: "",
+  });
+
+  const inputEvent = (event) => {
+    console.log(event.target.value);
+    console.log(event.target.name);
+
+    const value = event.target.value;
+    const name = event.target.name;
+
+    setFullName((preValue) => {
+      // console.log(prevalue)
+      if (name === "fName") {
+        return {
+          fname: value,
+          lname: preValue.lname,
+        };
+      } else if (name === "lName") {
+        return {
+          fname: preValue.fname,
+          lname: value,
+        };
+      }
+    });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    alert("Form Submitted");
+  };
+
+  return (
+    <>
+      <from>
+        <div>
+          <h1>
+            Hello {fullName.fname} {fullName.lname}
+          </h1>
+          <input
+            onChange={inputEvent}
+            name="fName"
+            type="text"
+            placeholder="Enter Your Name"
+            // value={fullName.fname}
+          />
+          <br />
+          <input
+            onChange={inputEvent}
+            name="lName"
+            type="text"
+            placeholder="Enter Your Lst Name"
+            // value={fullName.lName}
+          />
+
+          <button type="submit" onClick={onSubmit}>
+            Click Me
+          </button>
+        </div>
+      </from>
+    </>
+  );
+};
+
+export default App;
