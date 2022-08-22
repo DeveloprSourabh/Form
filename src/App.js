592a607cc4fe42d745568694a5743da275a3dc08
Sourabh <developr.sourabh@gmail.com>
2022-08-22 19:29:20 +0530

    React Form is completed

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ const App = () => {
   const [fullName, setFullName] = useState({
     fname: "",
     lname: "",
+    email: "",
+    phone: "",
   });
 
   const inputEvent = (event) => {
     console.log(event.target.value);
     console.log(event.target.name);
 
-    const value = event.target.value;
-    const name = event.target.name;
+    // const value = event.target.value;
+    // const name = event.target.name;
+
+    const { value, name } = event.target;
 
     setFullName((preValue) => {
       // console.log(prevalue)
@@ -20,11 +24,29 @@ const App = () => {
         return {
           fname: value,
           lname: preValue.lname,
+          email: preValue.email,
+          phone: preValue.phone,
         };
       } else if (name === "lName") {
         return {
           fname: preValue.fname,
           lname: value,
+          email: preValue.email,
+          phone: preValue.phone,
+        };
+      } else if (name === "email") {
+        return {
+          fname: preValue.fname,
+          lname: preValue.lname,
+          email: value,
+          phone: preValue.phone,
+        };
+      } else if (name === "phone") {
+        return {
+          fname: preValue.fname,
+          lname: preValue.lname,
+          email: preValue.email,
+          phone: value,
         };
       }
     });
@@ -42,22 +64,39 @@ const App = () => {
           <h1>
             Hello {fullName.fname} {fullName.lname}
           </h1>
+          <p>{fullName.email}</p>
+          <p>{fullName.phone}</p>
+
           <input
             onChange={inputEvent}
             name="fName"
             type="text"
-            placeholder="Enter Your Name"
-            // value={fullName.fname}
+            placeholder="Enter Your First Name"
+            value={fullName.fname}
           />
           <br />
           <input
             onChange={inputEvent}
             name="lName"
             type="text"
-            placeholder="Enter Your Lst Name"
-            // value={fullName.lName}
+            placeholder="Enter Your Last Name"
+            value={fullName.lname}
+          />
+          <input
+            onChange={inputEvent}
+            name="email"
+            type="email"
+            placeholder="Enter Your Email Adress"
+            value={fullName.email}
           />
 
+          <input
+            onChange={inputEvent}
+            name="phone"
+            type="number"
+            placeholder="Enter Your Mobile Number"
+            value={fullName.phone}
+          />
           <button type="submit" onClick={onSubmit}>
             Click Me
           </button>
